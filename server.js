04083e678aa3291bf7ebd931268f2877851f62de
Marko Vucetic <marko.vucetic@riitech.com>
2018-11-27 10:54:27 +0100

    server flags

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+    port: 3010,
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+    onStdout: function (data) {
+        // console.log('ee' + data)
+    },
+    onStderr: function (data) {
+        // console.error(data)
+    }
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
